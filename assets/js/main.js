aab7d8202944fb0f97cf960cbbafa7d20b508c8c
Juan Morales Ruiz <jmoralesr.dev@gmail.com>
2024-12-27 04:06:02 -0500

    feat(frontend): add music and verse section

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+// Poner audio a la página al entrar
+function playAudio() {
+    document.getElementById("bg-music").play();
+}
+
+// function stopAudio() {
+//     document.getElementById("bg-music").pause();
+// }
