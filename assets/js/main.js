f9509d213653ddf61e6aee818e894ef8f3c2dda4
Juan Morales Ruiz <jmoralesr.dev@gmail.com>
2024-12-29 02:17:04 -0500

    feat(frontend): add invitation and countdown

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,32 @@ function playAudio() {
 // function stopAudio() {
 //     document.getElementById("bg-music").pause();
 // }
+
+// Funcionalidad Cuenta regresiva
+const day = document.getElementById("days")
+const hour = document.getElementById("hours")
+const minute = document.getElementById("minutes")
+const second = document.getElementById("seconds")
+
+const wedding = '07 Mar 2025'
+
+function countTimer() {
+    const weddingDate = new Date(wedding);
+    const currentDate = new Date();
+
+    const totalSeconds = (weddingDate - currentDate) / 1000;
+
+    const daysCalc = Math.floor(totalSeconds / 3600 / 24);
+    const hoursCalc = Math.floor(totalSeconds / 3600) % 24;
+    const minutesCalc = Math.floor(totalSeconds / 60) % 60;
+    const secondsCalc = Math.floor(totalSeconds % 60);
+
+    day.innerHTML = daysCalc;
+    hour.innerHTML = hoursCalc;
+    minute.innerHTML = minutesCalc;
+    second.innerHTML = secondsCalc;
+}
+
+countTimer();
+
+setInterval(countTimer, 1000);
